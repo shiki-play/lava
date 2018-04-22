b1272ad80d4d48704da8259593a974d903a5883b
shiki <1401315109@qq.com>
2018-04-22 21:58:14 +0800

    优化了样式使用了npm

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title','Sample')</title>
+    <title>@yield('title','Sample App') - Laravel </title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
+  <header class="navbar navbar-fixed-top navbat-inverse">
+      <div class="container">
+          <div class="col-md-offset-1 col-md-10">
+              <a href="/" id="logo">Sampple App</a>
+              <nav>
+                  <ul class="nav navbar-nav navbar-right">
+                      <li><a href="/help">Help</a> </li>
+                      <li><a href="#">login</a></li>
+                  </ul>
+              </nav>
+          </div>
+      </div>
+  </header>
+  <div class="container">
     @yield('content')
+  </div>
+
   </body>
 </html>
