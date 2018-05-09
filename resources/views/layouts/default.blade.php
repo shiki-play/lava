14b816190d8f3bee972dd71e661591bfb0f664fd
shiki <1401315109@qq.com>
2018-05-09 21:15:24 +0800

    Finish login and logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,14 +6,15 @@
   </head>
   <body>
   @include('layouts._header')
-  </header>
+
   <div class="container">
   <div class="col-md-offset-1 col-md-10">
+  @include('shared._messages')
    @yield('content')
-    @include('layouts._footer')
     @include('layouts._footer')
   </div>
  </div>
-      
+
+      <script src="/js/app.js"></script>
   </body>
 </html>
