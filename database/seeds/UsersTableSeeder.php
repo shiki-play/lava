888dfb2a31c3ea481156ca95f262c806d60d1f17
shiki <1401315109@qq.com>
2018-05-13 17:04:34 +0800

    Add account activations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
          $user->email = '1401315109@qq.com';
          $user->password = bcrypt('123456');
          $user->is_admin = true;
+         $user->activated = true;
          $user->save();
     }
 }
